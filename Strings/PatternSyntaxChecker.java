0c103d43bf231ff104da17a6855cef78f0b3886e
Krishna Pala <palakrishnasai@gmail.com>
2021-05-12 10:52:36 +0530

    Create PatternSyntaxChecker.java
    Output Format
    
    For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
    
    Sample Input
    
    3
    ([A-Z])(.+)
    [AZ[a-z](a-z)
    batcatpat(nat
    Sample Output
    
    Valid
    Invalid
    Invalid

---
+++ Strings/PatternSyntaxChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * @author krishnasai21
+ *
+ */
+public class PatternSyntaxChecker {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases-- > 0) {
+			String pattern = in.nextLine();
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (Exception e) {
+				System.out.println("Invalid");
+			}
+		}
+		in.close();
+	}
+}
